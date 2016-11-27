f1f0ad5ff6d62db7371c8453a34966b3542f0948
Ilya Perevoznik <ilya@perevoznik.name>
2016-11-28 02:06:09 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 UPLOAD_FOLDER = '/Volumes/multimedia/prvz/Desktop/UNIVERSITY/SynVoS/app/static/audio'
 ALLOWED_EXTENSIONS = set(['wav'])
-
+YANDEX_API_KEY = 'cd6d68b2-8abf-4e88-9f91-881624e8392e'
 
 app = Flask(__name__)
 
