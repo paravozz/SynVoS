ddbb87c9940e77879e901dc4ca8b67b525d48032
Ilya Perevoznik <ilya@perevoznik.name>
2016-11-28 12:53:31 +0300

    add voice-generation

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 #!flask/bin/python3
 from markupsafe import Markup
 
-from app import app, ALLOWED_EXTENSIONS, UPLOAD_FOLDER
+from app import app, ALLOWED_EXTENSIONS, UPLOAD_FOLDER,YANDEX_API_KEY
 from flask import render_template, request, flash, redirect, url_for, jsonify
-import os
+import os, requests
 from werkzeug.utils import secure_filename
 from .controllers.WaveArray import WaveArray
 
@@ -13,40 +13,67 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/file_upload', methods=['GET', 'POST'])
+@app.route('/file_upload', methods=['POST'])
 def file_upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            # return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            # return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            path = os.path.join(UPLOAD_FOLDER, filename)
-            file.save(path)
-            wav = WaveArray(path)
-            # wav.time_stretch(2)
-            # wav.pitch_shift(-5)
-            # wav.save(os.path.join(UPLOAD_FOLDER, 'result.wav'))
-            # return Markup(wav.html_repr())
-            # flash(Markup(wav.html_repr()))
-            res = {
-                'represent': wav.html_repr(),
-                'file': wav.file_path,
-                'bpm': wav.bpm,
-                'stereo': wav.stereo,
-                'bars': wav.bars,
-                'bar_len': wav.bar_len
-            }
-            return jsonify(res)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        # return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        # return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(path)
+        wav = WaveArray(path)
+        # wav.time_stretch(2)
+        # wav.pitch_shift(-5)
+        # wav.save(os.path.join(UPLOAD_FOLDER, 'result.wav'))
+        # return Markup(wav.html_repr())
+        # flash(Markup(wav.html_repr()))
+        res = {
+            'represent': wav.html_repr(),
+            # 'file': wav.file_path,
+            'file': 'static/audio/' + filename,
+            'bpm': wav.bpm,
+            'stereo': wav.stereo,
+            'bar_count': wav.bars,
+            'bar_len': wav.bar_len
+        }
+        return jsonify(res)
+
+
+@app.route('/text-generated', methods=['POST'])
+def get_data():
+    if request.json:
+        req = request.json
     else:
-        return redirect(url_for('index'))
+        return 'Error'
+    audios = []
+    url = 'https://tts.voicetech.yandex.net/generate'
+
+    for line in req['lines']:
+        request_str = {
+            'text': line,
+            'format': 'wav',
+            'lang': req['lang'],
+            'speaker': req['speaker'],
+            'key': YANDEX_API_KEY
+        }
+        audios += [requests.get(url, request_str).content]
+
+    i = 1
+    for audio in audios:
+        with open(os.path.join(UPLOAD_FOLDER,
+                               '{}.wav'.format(i)), 'wb') as f:
+            f.write(audio)
+        i += 1
+
+    return 'good'
 
 
 @app.route('/', methods=['GET', 'POST'])
